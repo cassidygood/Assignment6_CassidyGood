92078ed85208ddc29839c2e92cd2dc184b47395f
cassidygood <cassidy.r.good-1@ou.edu>
2025-04-05 22:08:25 -0700

    added more scripts
    added a raycast to the gun so the enemy disappears when it comes into field

---
+++ Assets/Scripts/RaycastShooter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class RaycastShooter : MonoBehaviour
+{
+    public float rayDistance = 100f;
+    public LayerMask raycastLayerMask; // Optional: set to Enemy layer in inspector
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left click
+        {
+            ShootRay();
+        }
+    }
+
+    void ShootRay()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, rayDistance, raycastLayerMask))
+        {
+            Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.green, 1f);
+
+            // Check if the hit object has the EnemyWander script
+            EnemyWander enemy = hit.collider.GetComponent<EnemyWander>();
+            if (enemy != null)
+            {
+                Destroy(enemy.gameObject); // Or disable with SetActive(false);
+            }
+        }
+    }
+}
+
